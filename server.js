8af71fa9340342fbda201637b8088e6bf5f440b8
ysaad01 <ysaad2325@gmail.com>
2022-04-23 15:22:23 -0400

    added app.get api route to db.json file

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) throw err
+    res.send(data);
+  })
 });
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
