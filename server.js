0d927c16b699ad6c2b100a2915594d61f495d0e8
ysaad01 <ysaad2325@gmail.com>
2022-04-23 19:45:12 -0400

    updated port variable and created images folder for app gif

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
