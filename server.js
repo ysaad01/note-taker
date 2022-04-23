4e591a36d02c2bca9f0f228b2b8290611ddcaae8
ysaad01 <ysaad2325@gmail.com>
2022-04-23 17:42:48 -0400

    added middleware to route to public folder

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
 
 const express = require("express");
 const app = express();
 const port = 3000;
 
-
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -16,6 +19,7 @@ app.get("/api/notes", (req, res) => {
     res.send(data);
   })
 });
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
